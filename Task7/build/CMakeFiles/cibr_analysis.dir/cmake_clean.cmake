82f18c79364bb3129d9e0e3b03a08c6b9f103927
Jheel Kamdar <144502229+jheel-kamdar@users.noreply.github.com>
2025-02-06 19:41:28 -0500

    Add files via upload

---
+++ Task7/build/CMakeFiles/cibr_analysis.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cibr_analysis.dir/cibr_analysis.cpp.o"
+  "CMakeFiles/cibr_analysis.dir/cibr_analysis.cpp.o.d"
+  "cibr_analysis"
+  "cibr_analysis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cibr_analysis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
